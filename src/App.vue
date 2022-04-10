85c68bbfc1945abcde0ad3e3d781d1050c176c77
Eden <gresseden@gmail.com>
2022-04-10 00:19:45 -0400

    finished class Suspense component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <Home />
+  <!-- <SplashScreen /> -->
+  <Suspense>
+    <template #default><Home /></template>
+    <template  #fallback><SplashScreen /></template>
+  </Suspense> 
 </template>
 
 <script>
-import Home from "@/components/Home.vue";
+import SplashScreen from "@/components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
 
 export default {
   components: {
-    Home,
+    // Home,
+    SplashScreen,
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"));
+          }, 2500);
+        })
+    ),
   },
 };
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,25 @@
 <template>
   <div>Hello world!</div>
+  <!-- <SplashScreen /> -->
+  <!-- <Home /> -->
 </template>
+
+
+<script>
+// import SplashScreen from "@/components/SplashScreen.vue";
+// import { defineAsyncComponent } from "vue";
+
+export default {
+  components: {
+    // SplashScreen,
+    //   Home: defineAsyncComponent(
+    //     () =>
+    //       new Promise((resolve) => {
+    //         setTimeout(() => {
+    //           resolve(require("./components/Home.vue"));
+    //         }, 2500);
+    //       })
+    //   ),
+  },
+};
+</script>

21fb83c6e18ed4b03b8b539a6a98601fd9ee81be
Eden <gresseden@gmail.com>
2022-04-11 23:05:43 -0400

    Finished class historial-component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,7 +20,10 @@
       </Resume>
     </template>
     <template #movements>
-      <Movements/>
+      <Movements
+        :movements="movements"
+      
+      />
     </template>
   </Layout>
  
@@ -31,7 +34,7 @@
 import Layout from "./Layout.vue";
 import Header from "./Header.vue";
 import Resume from "./Resume/Index.vue";
-import Movements from "./Movements.vue";
+import Movements from "./Movements/Index.vue";
 
 export default {
   components: {
@@ -44,6 +47,38 @@ export default {
     return {
       amount: null,
       label: null,
+      movements: [
+        {
+          id: 1,
+          title: "Movement 1",
+          description: "lorem ipsum",
+          amount: 100,
+        },
+        {
+          id: 2,
+          title: "Movement 2",
+          description: "lorem ipsum",
+          amount: 200,
+        },
+        {
+          id: 3,
+          title: "Movement 3",
+          description: "lorem ipsum ",
+          amount: 300,
+        },
+        {
+          id: 4,
+          title: "Movement 4",
+          description: "lorem sdfsdfds ",
+          amount: 400,
+        },
+        {
+          id: 5,
+          title: "Movement 5",
+          description: "loremd hdjfkhjksdf ",
+          amount: 500,
+        },
+      ],
     };
   },
 };
